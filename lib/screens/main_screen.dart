df2c9c2e7097b72edc3aa8e051877571b9782b2e
fatyaahv <fatima.humbatli@gmail.com>
2026-01-14 00:21:08 +0300

    Publish project snapshot

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -16,7 +16,7 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   int _currentIndex = 0;
 
-  final List<Widget> _screens = const [
+  final List<Widget> _screens =  [
     DashboardScreen(),
     ControlScreen(),
     NotificationsScreen(),
